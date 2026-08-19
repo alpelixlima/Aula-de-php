1fab2a3a27f93f0cf31c16e0089ea65ca75085ed
ALPELIX WALLACE DA SILVA LIMA <20241134010028@ifrn.local>
2026-08-19 17:50:24 -0300

    subindo as primeiras questoes

---
+++ index(1).php
@@ -0,0 +1,39 @@
+<?php
+
+#questao [1]
+
+$numero = 1;
+
+while ($numero <= 50) {
+    echo $numero . "\n";
+    $numero++;
+} 
+    echo " fim da contagem.";
+
+
+?> 
+
+
+
+<?php
+#questao [2]
+
+for ($i = 0; $i <= 30; $i++) {
+    if($i % 2 == 0) {
+        echo "$i - par\n";
+    }else {
+        echo "$i - impa\n";
+    }
+
+}
+?> 
+
+
+
+
+<?php
+#questao [3]
+
+
+
+?>
